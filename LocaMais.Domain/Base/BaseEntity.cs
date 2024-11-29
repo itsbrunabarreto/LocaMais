d8316cc9d23c1bdf4de5462ec3cbae4ff0097ffd
bruna <bruna@BRUNA_BARRETO>
2024-11-29 11:25:46 -0300

    Adicionar arquivos de projeto.

---
+++ LocaMais.Domain/Base/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocaMais.Domain.Base
+{
+    public abstract class BaseEntity<TID>
+    {
+        protected BaseEntity()
+        {
+
+        }
+        protected BaseEntity(TID id)
+        {
+            Id = id;
+        }
+        public TID? Id { get; set; }
+    }
+}
